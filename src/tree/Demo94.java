ea939bad1242b8b68a8d1350010dbe4481497348
penguin <850728615@qq.com>
2022-04-25 18:02:46 +0800

    no changes

--- src/tree/Demo94.java
+++ src/tree/Demo94.java
@@ -1,7 +1,10 @@
 package tree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Demo94 {
     static class TreeNode {
@@ -23,12 +26,68 @@ public class Demo94 {
         }
     }
 
+    /**
+     * 中序遍历，左 -> 中 -> 右
+     *
+     * @param root
+     * @return
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
         TreeNode node = root;
-        while(node.left != null){
-            node = node.left;
-            if (node.left == null) res.add(node.val);
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
+        }
+        return res;
+    }
+
+    /**
+     * 前序遍历 中 -> 左 -> 右
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                res.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        return res;
+    }
+
+    public List<Integer> afterOrderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> stackH = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            if (node.right == null) {
+                res.add(node.val);
+                node = null;
+            }else{
+                stackH.push(node);
+                node = node.right;
+            }
         }
         return res;
     }
